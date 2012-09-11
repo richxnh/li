c74b845f11025dfa7a7578841797b3144588c65c
li <limw@w.cn>
2012-09-11 10:36:39 +0800

    添加TransTest

--- test/li/AllDaoTest.java
+++ test/li/AllDaoTest.java
@@ -5,13 +5,14 @@ import li.dao.PageTest;
 import li.dao.QueryBuilderTest;
 import li.dao.RecordTest;
 import li.dao.RecordTransTest;
+import li.dao.TransTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PageTest.class, RecordTest.class, RecordTransTest.class, DaoOfRecordTest.class, QueryBuilderTest.class })
+@SuiteClasses({ PageTest.class, RecordTest.class, RecordTransTest.class, DaoOfRecordTest.class, QueryBuilderTest.class, TransTest.class })
 public class AllDaoTest {
 
 }
---
+++ test/li/dao/TransTest.java
@@ -0,0 +1,40 @@
+package li.dao;
+
+import li.annotation.Inject;
+import li.test.BaseTest;
+
+import org.junit.Test;
+
+import com.cduer.forum.record.Account;
+
+public class TransTest extends BaseTest {
+	@Inject
+	Account accountDao;
+
+	@Test
+	public void testTrans() {
+		new Trans() {
+			public void run() {
+				System.err.println("trans 1 start");
+				accountDao.list(null);
+				new Trans() {
+					public void run() {
+						System.err.println("trans 2 start");
+						accountDao.list(null);
+						new Trans() {
+							public void run() {
+								System.err.println("trans 3 start");
+								accountDao.list(null);
+								System.err.println("trans 3 end");
+							}
+						};
+						accountDao.list(null);
+						System.err.println("trans 2 end");
+					}
+				};
+				accountDao.list(null);
+				System.err.println("trans 1 end");
+			}
+		};
+	}
+}
