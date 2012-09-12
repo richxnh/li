535a42f420cdd9c401c5f39649046fb50d70b559
limingwei@mail.com <limingwei@mail.com>
2012-09-13 00:29:10 +0800

    增加两个测试类

---
+++ test/li/dao/AbstractDaoTest.java
@@ -0,0 +1,12 @@
+package li.dao;
+
+import org.junit.Test;
+
+import li.test.BaseTest;
+
+public class AbstractDaoTest extends BaseTest {
+	@Test
+	public void test() {
+		System.out.println("AbstractDaoTest.test()");
+	}
+}
---
+++ test/li/dao/QueryRunnerTest.java
@@ -0,0 +1,12 @@
+package li.dao;
+
+import org.junit.Test;
+
+import li.test.BaseTest;
+
+public class QueryRunnerTest extends BaseTest {
+	@Test
+	public void test() {
+		System.out.println("QueryRunnerTest.test()");
+	}
+}
