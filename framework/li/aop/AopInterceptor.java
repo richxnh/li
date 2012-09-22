0cc3ae6fe6390248b88a054d6c4986e01d5098fa
limingwei@mail.com <limingwei@mail.com>
2012-09-22 10:09:06 +0800

    修改行内注释

--- framework/li/aop/AopInterceptor.java
+++ framework/li/aop/AopInterceptor.java
@@ -38,7 +38,7 @@ public class AopInterceptor {
 	public static Object getInstance(Class<?> type) {
 		// 构造这个类型所有方法的AopFilter的集合
 		final Map<Method, List<AopFilter>> filtersMap = new HashMap<>();
-		for (Method method : type.getDeclaredMethods()) {
+		for (Method method : type.getDeclaredMethods()) {// 对每一个方法
 			List<AopFilter> filters = new ArrayList<>();
 			Aop aop = method.getAnnotation(Aop.class);
 			for (int i = 0; null != aop && i < aop.value().length; i++) {// 如果有@Aop注解,对每一个@Aop.value()的值
@@ -54,9 +54,9 @@ public class AopInterceptor {
 		enhancer.setSuperclass(type);
 		enhancer.setCallback(new MethodInterceptor() {// 设置callback,使用AopChain代理执行方法
 			public Object intercept(Object target, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-				return new AopChain(target, method, args, filtersMap.get(method), proxy).doFilter().getResult();// 使用AopChian代理执行这个方法并返回值
-			}
+				return new AopChain(target, method, args, filtersMap.get(method), proxy).doFilter().getResult();
+			}// 使用AopChian代理执行这个方法并返回值
 		});
-		return enhancer.create();
+		return enhancer.create();// 创建代理对象
 	}
 }
