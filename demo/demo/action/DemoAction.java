f9bfe364c178a1cb52e63a483c6f590b32264aef
limingwei@mail.com <limingwei@mail.com>
2012-09-22 19:38:02 +0800

    更新测试代码

--- demo/demo/action/DemoAction.java
+++ demo/demo/action/DemoAction.java
@@ -7,10 +7,13 @@ import java.io.FileOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import li.annotation.Aop;
 import li.annotation.Arg;
 import li.annotation.At;
 import li.annotation.Bean;
 import li.annotation.Inject;
+import li.annotation.Trans;
+import li.aop.LogFilter;
 import li.mvc.AbstractAction;
 import li.mvc.Context;
 import li.util.Page;
@@ -137,6 +140,8 @@ public class DemoAction extends AbstractAction {
 	 * 返回freemarker视图
 	 */
 	@At("fm")
+	@Trans
+	@Aop({ LogFilter.class })
 	public void testFreemarker() {
 		setRequest("str1", "床前明月光 testFreemarker");
 		Page page = new Page();
