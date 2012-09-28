0a03b735b31f07d2dcf884a7bcc8536d5d20ae39
li <limw@w.cn>
2012-09-28 16:09:42 +0800

    更新Log中引号失误

--- framework/li/dao/QueryRunner.java
+++ framework/li/dao/QueryRunner.java
@@ -45,7 +45,7 @@ public class QueryRunner {
 		ResultSet resultSet = null;
 		if (null == Trans.CONNECTION_MAP.get() || null == Trans.EXCEPTION.get()) {
 			try { // 如果未进入事务或事务中未出现异常,则执行后面的语句
-				log.info(sql + " -> ”+connection.getClass().getName()+”@" + Integer.toHexString(connection.hashCode()));
+				log.info(sql + "-> " + connection.getClass().getName() + "@" + Integer.toHexString(connection.hashCode()));
 				preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 				resultSet = preparedStatement.executeQuery();
 			} catch (Exception e) {
@@ -63,7 +63,7 @@ public class QueryRunner {
 		Integer count = -1;
 		if (null == Trans.CONNECTION_MAP.get() || null == Trans.EXCEPTION.get()) {
 			try { // 如果未进入事务或事务中未出现异常,则执行后面的语句
-				log.info(sql + " -> ”+connection.getClass().getName()+”@" + Integer.toHexString(connection.hashCode()));
+				log.info(sql + " -> " + connection.getClass().getName() + "@" + Integer.toHexString(connection.hashCode()));
 
 				preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);// 构建要返回GeneratedKeys的Statement
 				count = preparedStatement.executeUpdate();
