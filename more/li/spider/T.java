c43434172866b7e9961e70e5faf9952caa6c34a4
limw <limw@w.cn>
2012-10-20 17:59:27 +0800

    ...

--- more/li/spider/T.java
+++ more/li/spider/T.java
@@ -1,5 +1,7 @@
 package li.spider;
 
+import java.util.Date;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,9 +9,37 @@ import org.jsoup.select.Elements;
 
 public class T {
 	public static void main(String[] args) throws Exception {
-		System.out.println("hello spaider");
+		final String url = "";
 
-		String url = "http://tieba.baidu.com/f?kw=%B3%C9%B6%BC%B4%F3%D1%A7";
+		for (int i = 0; i < 10000; i++) {
+			new Thread() {
+				public void run() {
+					try {
+						for (int j = 0;; j++) {
+							Document document = Jsoup.connect(url).get();
+							System.out.println(new Date() + "\t" + Thread.currentThread() + "\t" + j + "\t" + document.text());
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				};
+			}.start();
+		}
+	}
+
+	public static void chengda_yaowen() throws Exception {
+		String url = "";
+		Document document = Jsoup.connect(url).get();
+
+		Elements threads = document.select("div.list ul li a");
+
+		for (Element thread : threads) {
+			System.out.println(thread.attr("href") + "\t" + thread.text());
+		}
+	}
+
+	public static void baidu_tieba() throws Exception {
+		String url = "";
 		Document document = Jsoup.connect(url).get();
 
 		Elements threads = document.select("a[class=j_th_tit]");
