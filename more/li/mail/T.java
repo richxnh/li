911354b5d5ad3df5a52ad941401a2b3cf3374b91
limw <limw@w.cn>
2012-10-20 17:01:28 +0800

    ...

--- more/li/mail/T.java
+++ more/li/mail/T.java
@@ -4,15 +4,15 @@ import org.apache.commons.mail.SimpleEmail;
 
 public class T {
 	public static void main(String[] args) throws Exception {
-		  SimpleEmail email = new SimpleEmail(); // 构造一个mail对象
-		  email.setHostName("smtp.qq.com");
-	      email.setFrom("416133823@qq.com");// 设置发送人
-		  email.setAuthentication("416133823@qq.com", "buxiaode");
-	      email.setSubject("邮件测试标题");// 设置主题
-	      email.setContent("测试内容是我自己的", "text/plain;charset=GBK");// 设置内容	
-	      email.addTo("cdusj@foxmail.com ");// 设置发对象
-	      email.send();// 发送
-	      
-	      System.out.println("done");
+		SimpleEmail email = new SimpleEmail(); // 构造一个mail对象
+		email.setHostName("smtp.qq.com");
+		email.setFrom("416133823@qq.com");// 设置发送人
+		email.setAuthentication("416133823@qq.com", "buxiaode");
+		email.setSubject("邮件测试标题");// 设置主题
+		email.setContent("测试内容是我自己的", "text/plain;charset=GBK");// 设置内容
+		email.addTo("cdusj@foxmail.com ");// 设置发对象
+		email.send();// 发送
+
+		System.out.println("done");
 	}
 }
--- more/li/spider/T.java
+++ more/li/spider/T.java
@@ -8,14 +8,14 @@ import org.jsoup.select.Elements;
 public class T {
 	public static void main(String[] args) throws Exception {
 		System.out.println("hello spaider");
-		
-		String url="http://tieba.baidu.com/f?kw=%B3%C9%B6%BC%B4%F3%D1%A7";
-		Document document=Jsoup.connect(url).get();
 
-		Elements threads=document.select("a[class=j_th_tit]");
-		
+		String url = "http://tieba.baidu.com/f?kw=%B3%C9%B6%BC%B4%F3%D1%A7";
+		Document document = Jsoup.connect(url).get();
+
+		Elements threads = document.select("a[class=j_th_tit]");
+
 		for (Element thread : threads) {
-			System.out.println(thread.attr("href")+"\t"+thread.text());
+			System.out.println(thread.attr("href") + "\t" + thread.text());
 		}
 	}
 }
