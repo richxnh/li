a57c1311945de7e90589ee25c5e290099ddffd15
li <limw@w.cn>
2012-10-22 17:41:21 +0800

    ...

---
+++ more/li/mail/Beetl.java
@@ -0,0 +1,32 @@
+package li.mail;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.bee.tl.core.Config;
+import org.bee.tl.core.Template;
+
+public class Beetl {
+	private Template template;
+
+	public Beetl(File file) {
+		try {
+			template = new Config().createGroupTemplate().getReaderTemplate(new FileReader(file));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String merge(Map<?, ?> map) {
+		try {
+			for (Entry<?, ?> entry : map.entrySet()) {
+				template.set(entry.getKey().toString(), entry.getValue());
+			}
+			return template.getTextAsString();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
--- more/li/mail/JavaMail.java
+++ more/li/mail/JavaMail.java
@@ -1,14 +1,23 @@
 package li.mail;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JavaMail {
 	public static void main(String[] args) {
 		Sender sender = new Sender("smtp.qq.com", "416133823@qq.com", "buxiaode");
+		Beetl beetl = new Beetl(new File("D:/workspace/li/more/li/mail/mail_1.htm"));
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.put("name", "黎明伟");
+		map.put("date", "2012-10-22");
+
 		Mail mail = new Mail();
 		mail.setFrom("416133823@qq.com");
 		mail.setTo("cdusj@qq.com");
 		mail.setSubject("静夜思");
-		mail.setContent("床前明月光");
-
+		mail.setContent(beetl.merge(map));
 		sender.send(mail);
 	}
 }
