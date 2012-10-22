8a102be856c497097c414000722a5ab78c239376
limingwei@mail.com <limingwei@mail.com>
2012-10-22 22:18:32 +0800

    ...

--- more/li/office/T.java
+++ more/li/office/T.java
@@ -1,6 +1,10 @@
 package li.office;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -10,18 +14,38 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 public class T {
 	public static void main(String[] args) throws Exception {
-		Workbook workbook = new HSSFWorkbook(new FileInputStream("F:\\PPS\\人力资源部\\2010会员档案总列表.xls"));
+		File file = new File("F:\\PPS\\人力资源部\\2010会员档案总列表.xls");
 
-		for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
-			Sheet sheet = workbook.getSheetAt(0);
-			int n = 0;
-			for (Row row : sheet) {
-				System.out.print(n++ + "\t");
-				for (Cell cell : row) {
-					System.out.print(cell + "\t");
-				}
-				System.out.println();
+		List<List<String>> table = read(file);
+
+		for (List<String> line : table) {
+			for (String cell : line) {
+				System.out.print(cell + "\t");
 			}
+			System.out.println();
+		}
+	}
+
+	public static List<List<String>> read(File file) {
+		try {
+
+			List<List<String>> table = new ArrayList<List<String>>();
+
+			Workbook workbook = new HSSFWorkbook(new BufferedInputStream(new FileInputStream(file)));
+
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				Sheet sheet = workbook.getSheetAt(0);
+				for (Row row : sheet) {
+					List<String> line = new ArrayList<String>();
+					for (Cell cell : row) {
+						line.add(cell.toString());
+					}
+					table.add(line);
+				}
+			}
+			return table;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
