6f589646725f004c01b10e9c3bcc749debc5ba06
li <limw@w.cn>
2012-10-22 16:34:35 +0800

    ...

--- more/li/mail/JavaMail.java
+++ more/li/mail/JavaMail.java
@@ -1,6 +1,5 @@
 package li.mail;
 
-import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -10,34 +9,32 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class JavaMail {
 	public static void main(String[] args) throws Exception {
 		Properties properties = new Properties();
 		properties.put("mail.smtp.host", "smtp.qq.com");
 		properties.put("mail.smtp.auth", true);
-
-		Authenticator authenticator = new Authenticator() {
+		Session session = Session.getDefaultInstance(properties, new Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication("416133823@qq.com", "buxiaode");
 			}
-		};
-
-		Session session = Session.getDefaultInstance(properties, authenticator);
+		});
 		session.setDebug(true);
-
 		Message message = new MimeMessage(session);
-		message.setSentDate(new Date());
-
 		message.setFrom(new InternetAddress("416133823@qq.com"));
-		message.setRecipient(RecipientType.TO, new InternetAddress("cdusj@qq.com"));
-
+		message.addRecipient(RecipientType.TO, new InternetAddress("cdusj@qq.com"));
 		message.setSubject("邮件标题");
-		message.setText("邮件内容");
-
+		MimeMultipart multipart = new MimeMultipart();
+		MimeBodyPart bodyPart = new MimeBodyPart();
+		bodyPart.setHeader("Content-Transfer-Encoding", "base64");
+		bodyPart.setContent("邮件内容", "text/html;charset=UTF-8");
+		multipart.addBodyPart(bodyPart);
+		message.setContent(multipart);
 		Transport.send(message);
-
 		System.out.println("done");
 	}
 }
