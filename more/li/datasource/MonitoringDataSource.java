19ba26a172df4dcf214f2679cc73e7c2f956a45f
li <limw@w.cn>
2012-10-31 14:57:06 +0800

    添加一个DataSource,可以监控为关闭的数据库链接

---
+++ more/li/datasource/MonitoringDataSource.java
@@ -0,0 +1,74 @@
+package li.datasource;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+public class MonitoringDataSource implements DataSource {
+    private String url;
+    private String username;
+    private String password;
+
+    private List<Connection> connections = new ArrayList<Connection>();
+
+    static {
+        String[] drivers = { "org.sqlite.JDBC" };
+        for (String driver : drivers) {
+            try {
+                Class.forName(driver);
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return getConnection(this.username, this.password);
+    }
+
+    public Connection getConnection(String username, String password) throws SQLException {
+        Connection connection = new MonitoringConnection(this, DriverManager.getConnection(this.url, username, password));
+        connections.add(connection);
+        return connection;
+    }
+
+    public void removeConnection(Connection connection) {
+        connections.remove(connection);
+        System.err.println("还有未关闭的链接" + connections.size() + "个");
+        for (Connection con : connections) {
+            System.err.println(con + "\t");
+        }
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+}
