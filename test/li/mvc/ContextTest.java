0e1769137448dfcd4ec02fd82e87f5fe50a3e0e9
li <limw@w.cn>
2012-11-01 10:14:48 +0800

    更新测试

--- test/li/mvc/ContextTest.java
+++ test/li/mvc/ContextTest.java
@@ -1,5 +1,6 @@
 package li.mvc;
 
+import static org.junit.Assert.assertNotNull;
 import li.test.ActionTest;
 
 import org.junit.Before;
@@ -8,16 +9,16 @@ import org.junit.Test;
 public class ContextTest extends ActionTest {
     @Before
     public void before() {
-        // Context.init(request, response, null);
+        Context.init(request, response, null);
     }
 
     @Test
     public void getRequest() {
-        // assertNotNull(Context.getRequest());
+        assertNotNull(Context.getRequest());
     }
 
     @Test
     public void getResponse() {
-        // assertNotNull(Context.getResponse());
+        assertNotNull(Context.getResponse());
     }
 }
--- test/li/util/VerifyTest.java
+++ test/li/util/VerifyTest.java
@@ -1,10 +1,42 @@
 package li.util;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import li.test.BaseTest;
 
 import org.junit.Test;
 
 public class VerifyTest extends BaseTest {
     @Test
-    public void test() {}
+    public void basicType() {
+        assertTrue(Verify.basicType(int.class));
+        assertTrue(Verify.basicType(Integer.class));
+        assertFalse(Verify.basicType(this.getClass()));
+    }
+
+    @Test
+    public void contain() {
+        assertTrue(Verify.contain("12345", "123"));
+        assertTrue(Verify.contain("ABCDE", "bCd"));
+    }
+
+    @Test
+    public void endWith() {
+        assertTrue(Verify.endWith("abcdefg", "efg"));
+    }
+
+    @Test
+    public void isEmpty() {
+        assertTrue(Verify.isEmpty("   "));
+    }
+
+    @Test
+    public void regex() {
+        assertTrue(Verify.regex("abcdefg", ".*"));
+    }
+
+    @Test
+    public void startWith() {
+        assertTrue(Verify.startWith("abcdefg", "abc"));
+    }
 }
