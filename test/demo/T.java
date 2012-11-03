1f50e372da1327884b996259bc75c212ac7505a0
limingwei@mail.com <limingwei@mail.com>
2012-11-03 09:56:12 +0800

    更新行内注释

--- test/demo/T.java
+++ test/demo/T.java
@@ -7,15 +7,15 @@ public class T {
     public static void main(String[] args) throws Exception {
         final _Account account = Ioc.get(_Account.class);
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1000; i++) {// 开启1000个线程
             new Thread() {
                 public void run() {
-                    for (; true;) {
-                        account.update("SET password=password");
+                    for (; true;) {// 每个线程无限循环
+                        account.update("SET password=password");// 这是一个带Aop带Trans的Dao方法
                     }
                 }
             }.start();
-            Thread.sleep(100);
+            Thread.sleep(100);// 每隔0.1秒增加一个线程
         }
     }
 }
