2839f8f4ce47ca83b35455e81f1588e7b3f69d74
li <limw@w.cn>
2012-11-05 09:32:35 +0800

    bugfix:PreparedStatement的npe

--- framework/li/dao/QueryRunner.java
+++ framework/li/dao/QueryRunner.java
@@ -85,7 +85,9 @@ public class QueryRunner {
      */
     public void close() {
         try {
-            preparedStatement.close();
+            if (null != preparedStatement) {
+                preparedStatement.close();
+            }
             if (null != connection && null == Trans.CONNECTION_MAP.get()) {
                 connection.close();// Trans.CONNECTION_MAP.get()为空表示未进入事务,若已进入事务,则由事务关闭连接
             }
