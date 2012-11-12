7e76cefd2691dfe6c51d3f4a1fc3eec922a83eae
li <limw@w.cn>
2012-11-12 16:46:08 +0800

    添加ehcache

---
+++ more/li/ehcache/Demo.java
@@ -0,0 +1,10 @@
+package li.ehcache;
+
+public class Demo {
+    /**
+     * 请求路径加QueryString为key,缓存request中的每一个属性
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
