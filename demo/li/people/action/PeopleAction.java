0ae1cc64e8e720a49fe926058de005d577ef9116
li <limw@w.cn>
2012-11-13 17:47:26 +0800

    people更新

--- demo/li/people/action/PeopleAction.java
+++ demo/li/people/action/PeopleAction.java
@@ -1,9 +1,19 @@
 package li.people.action;
 
+import li.annotation.At;
 import li.annotation.Bean;
+import li.annotation.Inject;
 import li.mvc.AbstractAction;
+import li.people.record.People;
+import li.util.Page;
 
 @Bean
 public class PeopleAction extends AbstractAction {
+    @Inject
+    People peopleDao;
 
+    @At("people_list.do")
+    public void list(Page page) {
+        setRequest("peoples", peopleDao.list(page)).view("people/list");
+    }
 }
