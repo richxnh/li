f969e79fa13e36523d662e42f010207f21e08556
黎明伟 <416133823@qq.com>
2012-11-17 14:07:02 +0800

    调整quartz测试

--- more/li/quartz/Demo.java
+++ more/li/quartz/Demo.java
@@ -1,12 +1,12 @@
 package li.quartz;
 
-import li.ioc.Ioc;
-
 public class Demo {
     /**
      * JUnit不支持多线程的测试,不能用于测试Quartz
      */
     public static void main(String[] args) {
-        System.out.println(Ioc.get(Quartz.class));// 在Xml中配置li.quartz.Quartz为一个Bean,可以使Ioc启动时启动Quartz
+        // Ioc.get(Quartz.class);// 在Xml中配置li.quartz.Quartz为一个Bean,可以使Ioc启动时启动Quartz
+
+        new Quartz();
     }
 }
