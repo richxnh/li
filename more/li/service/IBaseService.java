3bca5e41ec0bc74417f4a07462553c0952e71e99
黎明伟 <416133823@qq.com>
2012-11-25 09:49:12 +0800

    添加Service到More

---
+++ more/li/service/IBaseService.java
@@ -0,0 +1,38 @@
+package li.service;
+
+import java.util.List;
+
+import li.util.Page;
+
+/**
+ * 基础Service接口,你可以继承并扩展它形成自己的Service接口
+ * 
+ * @author li (limw@w.cn)
+ * @version 0.1.2 (2012-06-26)
+ */
+public interface IBaseService<T> {
+    /**
+     * 删除
+     */
+    public Boolean delete(Number id);
+
+    /**
+     * 查找
+     */
+    public T find(Number id);
+
+    /**
+     * 列表
+     */
+    public List<T> list(Page page);
+
+    /**
+     * 保存
+     */
+    public Boolean save(T t);
+
+    /**
+     * 更新
+     */
+    public Boolean update(T t);
+}
