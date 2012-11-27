fdc4ca08e609292f00da9ac3e9ed940af3d0ecec
黎明伟 <416133823@qq.com>
2012-11-27 21:49:38 +0800

    添加CacheFilter

---
+++ more/li/ehcache/CacheFilter.java
@@ -0,0 +1,10 @@
+package li.ehcache;
+
+import li.aop.AopChain;
+import li.aop.AopFilter;
+
+public class CacheFilter implements AopFilter {
+    public void doFilter(AopChain chain) {
+        
+    }
+}
