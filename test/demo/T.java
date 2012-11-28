0d35800ed2660863fa8e9e7bdea30eda36fe126c
黎明伟 <416133823@qq.com>
2012-11-28 23:56:13 +0800

    druid测试

--- test/demo/T.java
+++ test/demo/T.java
@@ -1,5 +1,13 @@
 package demo;
 
+import li.ioc.Ioc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
 public class T {
-    public static void main(String[] args) throws Exception {}
+    public static void main(String[] args) throws Exception {
+        DruidDataSource druidDataSource = Ioc.get(DruidDataSource.class);
+
+        System.out.println(druidDataSource.getConnection());
+    }
 }
