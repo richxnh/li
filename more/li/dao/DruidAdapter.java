cbd6c708e3f5e12178be66829376579bbd072e22
li <limw@w.cn>
2012-11-29 19:44:26 +0800

    Druid改名为DruidAdapter

--- more/li/dao/DruidAdapter.java
+++ more/li/dao/DruidAdapter.java
@@ -3,37 +3,37 @@ package li.dao;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
+
+import li.util.Files;
 
 import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.wall.WallConfig;
 import com.alibaba.druid.wall.WallFilter;
 
-public class Druid extends DruidDataSource {
+public class DruidAdapter extends DruidDataSource {
     private static final long serialVersionUID = 8840084696561752691L;
 
+    private static Properties properties = Files.load("config.properties");
+
     // 禁止使用的func列表
-    private static final String[] PERMIT_FUNCTIONS = {};
-
+    private static String[] PERMIT_FUNCTIONS = properties.getProperty("DRUID.PERMIT_FUNCTIONS").split(",");
     // 禁止访问的数据库列表
-    private static final String[] PERMIT_SCHEMAS = {};
-
+    private static String[] PERMIT_SCHEMAS = properties.getProperty("DRUID.PERMIT_SCHEMAS").split(",");
     // 禁止访问的table列表
-    private static final String[] PERMIT_TABLES = {};
-
+    private static String[] PERMIT_TABLES = properties.getProperty("DRUID.PERMIT_TABLES").split(",");
     // 只读table列表
-    private static final String[] READONLY_TABLES = {};
-
+    private static String[] READONLY_TABLES = properties.getProperty("DRUID.READONLY_TABLES").split(",");
     // 禁止访问的系统对象列表
-    private static final String[] PERMIT_OBJECTS = {};
-
+    private static String[] PERMIT_OBJECTS = properties.getProperty("DRUID.PERMIT_OBJECTS").split(",");
     //
-    private static final String[] PERMIT_VARIANTS = {};
+    private static String[] PERMIT_VARIANTS = properties.getProperty("DRUID.PERMIT_VARIANTS").split(",");
 
     /**
      * 初始化方法
      */
-    public Druid() {
+    public DruidAdapter() {
         WallConfig wallConfig = loadWallConfig();
         WallFilter wallFilter = new WallFilter();
         wallFilter.setConfig(wallConfig);
