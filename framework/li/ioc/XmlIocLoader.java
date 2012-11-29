e37b1320f5177c1fad649e6931fa8989b074a550
黎明伟 <416133823@qq.com>
2012-11-29 21:38:31 +0800

    整理及小调整

--- framework/li/ioc/XmlIocLoader.java
+++ framework/li/ioc/XmlIocLoader.java
@@ -46,9 +46,6 @@ public class XmlIocLoader {
                     Field field = new Field();// 一个新的Field
                     field.name = (String) Files.xpath(propertyNodes.item(m), "@name", XPathConstants.STRING);
                     field.type = Reflect.fieldType(iocBean.type, field.name);
-                    if (null == field.type) {// 配置文件中把属性名写错了
-                        throw new RuntimeException("Field " + field.name + " not found" + " in class " + type + " , which is configured in " + filePath);
-                    }
                     field.value = (String) Files.xpath(propertyNodes.item(m), "@value", XPathConstants.STRING);
                     iocBean.fields.add(field);
                 }
