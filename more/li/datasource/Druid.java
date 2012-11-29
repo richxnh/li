330304d224e0e27f92e62e471d18ccd15b59aa39
li <limw@w.cn>
2012-11-29 13:47:33 +0800

    druid适配更新

--- more/li/datasource/Druid.java
+++ more/li/datasource/Druid.java
@@ -1,9 +1,62 @@
 package li.datasource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
 
 public class Druid extends DruidDataSource {
     private static final long serialVersionUID = 8840084696561752691L;
 
-    private String filters;
+    // 禁止使用的func列表
+    private static final String[] PERMIT_FUNCTIONS = {};
+
+    // 禁止访问的数据库列表
+    private static final String[] PERMIT_SCHEMAS = {};
+
+    // 禁止访问的table列表
+    private static final String[] PERMIT_TABLES = {};
+
+    // 只读table列表
+    private static final String[] READONLY_TABLES = {};
+
+    // 禁止访问的系统对象列表
+    private static final String[] PERMIT_OBJECTS = {};
+
+    //
+    private static final String[] PERMIT_VARIANTS = {};
+
+    /**
+     * 初始化方法
+     */
+    public Druid() {
+        WallConfig wallConfig = loadWallConfig();
+        WallFilter wallFilter = new WallFilter();
+        wallFilter.setConfig(wallConfig);
+
+        List<Filter> filters = new ArrayList<Filter>();
+        filters.add(wallFilter);
+
+        super.setProxyFilters(filters);
+    }
+
+    /**
+     * 加在WallConfig配置
+     */
+    private WallConfig loadWallConfig() {
+        WallConfig wallConfig = new WallConfig();
+
+        wallConfig.getPermitFunctions().addAll(Arrays.asList(PERMIT_FUNCTIONS));
+        wallConfig.getPermitObjects().addAll(Arrays.asList(PERMIT_OBJECTS));
+        wallConfig.getPermitSchemas().addAll(Arrays.asList(PERMIT_SCHEMAS));
+        wallConfig.getPermitTables().addAll(Arrays.asList(PERMIT_TABLES));
+        wallConfig.getPermitVariants().addAll(Arrays.asList(PERMIT_VARIANTS));
+        wallConfig.getReadOnlyTables().addAll(Arrays.asList(READONLY_TABLES));
+
+        return wallConfig;
+    }
 }
--- test/demo/T.java
+++ test/demo/T.java
@@ -6,6 +6,9 @@ import li.ioc.Ioc;
 public class T {
     public static void main(String[] args) throws Exception {
         _User userDao = Ioc.get(_User.class);
-        System.out.println(userDao.query(null, "select version()"));
+
+        System.out.println(userDao.list(null, "select * from framework_forum.t_account"));
+        System.out.println(userDao.list(null, "select * from mysql.db"));
+        System.out.println(userDao.list(null, "select version()"));
     }
 }
